8e49fe149b5323990ba0c56dc3257898907c34aa
Tina Sumilova <tina.sumilova@accenture.com>
2022-09-19 14:41:18 +0300

    testing.commit

--- test2/src/Main.java
+++ test2/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        int a,b;
+        a= 3;
+        b =5;
+        System.out.println("res = " + a+b);
         System.out.println("Hello world2!");
     }
 }
